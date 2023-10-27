e5624c4fe4c6587fd9b056d3b72f4bc08d549726
Daniel Mendes <daniel.mendes@metropoles.com>
2023-10-27 00:08:15 -0300

    Criando primeiro Story e configurando o GlobalStyles no Preview

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  parameters: {
+    layout: 'full'
+  }
+} as Meta
+
+export const Default: StoryObj = {}
